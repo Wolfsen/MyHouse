5bdc09604b4e54062cf7063df1024ec0cfca4097
novik <novik@DESKTOP-K1FLG14>
2017-09-12 18:10:20 +0500

    Переименование формы

--- MainMenu.cs
+++ MainMenu.cs
@@ -10,12 +10,11 @@ using System.Windows.Forms;
 
 namespace MyHouse
 {
-    public partial class Form1 : Form
+    public partial class MainMenu : Form
     {
-        public Form1()
+        public MainMenu()
         {
             InitializeComponent();
-            ппп
         }
     }
 }
